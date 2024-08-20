5a767a7d5caf6504b0f450df9745b4e77fef31a0
FranGuam <frankguansy@icloud.com>
2024-08-20 22:05:53 +0800

    feat(05): ChatBox & FileShare

--- frontend/src/ChatBox.tsx
+++ frontend/src/ChatBox.tsx
@@ -84,9 +84,9 @@ const ChatBox: React.FC<ChatBoxProps> = ({ user, room, handleClose }) => {
       </Container>
       <MessageFeed user={user} messages={data?.message} />
       <div
-        className="need-interaction"
         style={{
-          marginTop: "12px",
+          margin: "12px",
+          marginBottom: 0,
           display: "flex",
           width: "100%",
         }}
