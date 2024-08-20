f1a0eb511c59490e167cd5366a2319d2972cd50f
FranGuam <frankguansy@icloud.com>
2024-08-20 16:21:28 +0800

    feat(05): brick and mortar

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -1,7 +1,3 @@
-import { Button as AntdButton, Typography } from "antd";
-
-const { Text: AntdText, Link: AntdLink } = Typography;
-
 export const Container: React.FC<
   React.PropsWithChildren<{ style?: React.CSSProperties }>
 > = ({ children, style }) => {
@@ -33,7 +29,6 @@ export const Card: React.FC<
         backgroundColor: "rgba(255, 255, 255, 0.25)",
         boxShadow: "0 0 18px rgba(0, 0, 0, 0.25)",
         backdropFilter: "blur(12px)",
-        WebkitBackdropFilter: "blur(12px)",
       }}
     >
       {children}
@@ -41,48 +36,9 @@ export const Card: React.FC<
   );
 };
 
-export const Bubble: React.FC<
-  React.PropsWithChildren<{ style?: React.CSSProperties }>
-> = ({ children, style }) => {
-  return (
-    <div
-      style={{
-        ...style,
-        padding: "6px",
-        margin: "6px",
-        borderRadius: "8px",
-        boxShadow: "0 0 6px rgba(0, 0, 0, 0.25)",
-        backdropFilter: "blur(6px)",
-        WebkitBackdropFilter: "blur(6px)",
-      }}
-    >
-      {children}
-    </div>
-  );
-};
-
-export const fontFamilies = [
-  "Times New Roman",
-  "Times",
-  "Nimbus Roman No9 L",
-  "Liberation Serif",
-  "FreeSerif",
-  "Hoefler Text",
-  "Microsoft YaHei", // 微软雅黑
-  "Hiragino Sans GB", // 冬青黑体
-  "WenQuanYi Micro Hei", // 文泉驿微米黑
-  "STHeiti", // 华文黑体
-  "sans-serif", // 无衬线
-];
-
 export const Text: React.FC<
-  React.PropsWithChildren<{
-    style?: React.CSSProperties;
-    size?: string;
-    editable?: any;
-    copyable?: any;
-  }>
-> = ({ children, style, size, editable, copyable }) => {
+  React.PropsWithChildren<{ style?: React.CSSProperties; size?: string }>
+> = ({ children, style, size }) => {
   switch (size) {
     case "small":
       size = "12px";
@@ -103,37 +59,14 @@ export const Text: React.FC<
       size = "18px";
   }
   return (
-    <AntdText
+    <span
       style={{
         ...style,
         fontSize: size,
       }}
-      editable={editable}
-      copyable={copyable}
     >
       {children}
-    </AntdText>
-  );
-};
-
-export const Link: React.FC<
-  React.PropsWithChildren<{
-    style?: React.CSSProperties;
-    onClick?: () => void;
-    danger?: boolean;
-  }>
-> = ({ children, style, onClick, danger }) => {
-  return (
-    <AntdLink
-      type={danger ? "danger" : undefined}
-      style={{
-        ...style,
-        fontSize: "12px",
-      }}
-      onClick={onClick}
-    >
-      {children}
-    </AntdLink>
+    </span>
   );
 };
 
@@ -141,8 +74,7 @@ export const Button: React.FC<
   React.PropsWithChildren<{ style?: React.CSSProperties; onClick?: () => void }>
 > = ({ children, style, onClick }) => {
   return (
-    <AntdButton
-      className="need-interaction"
+    <button
       style={{
         ...style,
         fontSize: "18px",
@@ -152,26 +84,6 @@ export const Button: React.FC<
       onClick={onClick}
     >
       {children}
-    </AntdButton>
-  );
-};
-
-export const Scroll: React.FC<
-  React.PropsWithChildren<{ style?: React.CSSProperties }>
-> = ({ children, style }) => {
-  return (
-    <div
-      className="need-interaction"
-      style={{
-        ...style,
-        height: "100%",
-        width: "100%",
-        overflowY: "scroll",
-        scrollbarWidth: "thin",
-        scrollbarColor: "rgba(0, 0, 0, 0.25) rgba(255, 255, 255, 0)",
-      }}
-    >
-      {children}
-    </div>
+    </button>
   );
 };
