13268b593918bb00e921e81dcffa77b2a65cdf4e
FranGuam <frankguansy@icloud.com>
2024-08-21 15:47:23 +0800

    feat(05): UX improvements

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -241,6 +241,7 @@ export const Scroll: React.FC<
 > = ({ children, style }) => {
   return (
     <div
+      className="need-interaction"
       style={{
         ...style,
         height: "100%",
